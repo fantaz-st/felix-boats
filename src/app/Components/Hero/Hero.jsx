dc0b87e3e405232455ba80d5e78cff65481d628f
fantaz-st <cbabic@pfst.hr>
2025-03-27 19:03:38 +0100

    Hero animation

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -1,10 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import Button from "../Button/Button";
 import classes from "./Hero.module.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import SplitType from "split-type";
+import { useRef } from "react";
 
 const Hero = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const splittedTitle = new SplitType(`.${classes.title}`, {
+        types: "chars",
+        tagName: "span",
+      });
+
+      const tl = gsap.timeline();
+      gsap.set(`.${classes.text}`, {
+        autoAlpha: 1,
+      });
+      tl.to(splittedTitle.chars, {
+        y: "0%",
+        stagger: 0.03,
+        duration: 0.75,
+      }).to(`.${classes.subtitle}`, {
+        opacity: 1,
+        duration: 0.5,
+      });
+    },
+    { scope: containerRef }
+  );
   return (
-    <div className={classes.container}>
+    <div className={classes.container} ref={containerRef}>
       <div className={classes.text}>
         <h1 className={classes.title}>Felix Boats</h1>
         <h3 className={classes.subtitle}>High-performance speed boats that turn heads and dominate the waves.</h3>
