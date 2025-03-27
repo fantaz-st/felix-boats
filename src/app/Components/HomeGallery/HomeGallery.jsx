cb1e2985f380a936ff79aa33021d3569b8d89921
fantaz-st <cbabic@pfst.hr>
2025-03-27 02:31:55 +0100

    gsap animating gallery

--- src/app/Components/HomeGallery/HomeGallery.jsx
+++ src/app/Components/HomeGallery/HomeGallery.jsx
@@ -1,23 +1,61 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import classes from "./HomeGallery.module.css";
 import homeGalleryImages from "./HomeGalleryImages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const HomeGallery = () => {
+  const containerRef = useRef(null);
+
+  // Initialize Fancybox
   useEffect(() => {
     Fancybox.bind("[data-fancybox='gallery']", {});
-
     return () => {
       Fancybox.destroy();
     };
   }, []);
 
+  /*  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      const items = gsap.utils.toArray(`.${classes.item}`);
+
+      items.forEach((item) => {
+        gsap.fromTo(
+          item,
+          // FROM
+          {
+            filter: "blur(8px)",
+            opacity: 0,
+            y: 25,
+          },
+          // TO
+          {
+            filter: "blur(0px)",
+            opacity: 1,
+            y: 0,
+            duration: 1.2,
+            scrollTrigger: {
+              trigger: item,
+              start: "top 80%",
+              toggleActions: "play none none none",
+            },
+          }
+        );
+      });
+    },
+    { scope: containerRef }
+  ); */
+
   return (
-    <div className={classes.container}>
+    <div className={classes.container} ref={containerRef}>
       <div className={classes.inner}>
         <div className={classes.text}>
           <h1 className={classes.title}>Excellence In Everything We Do</h1>
@@ -32,7 +70,6 @@ const HomeGallery = () => {
             </div>
           ))}
         </div>
-        {/* lol */}
       </div>
     </div>
   );
