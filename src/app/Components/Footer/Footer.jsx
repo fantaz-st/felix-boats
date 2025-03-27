70a233ae6add6f2048b7d4790df9e7311bb11648
fantaz-st <cbabic@pfst.hr>
2025-03-27 01:50:37 +0100

    hero size, footer more info

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -3,20 +3,26 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={classes.container}>
-      <h3>OBRAZ d.o.o.</h3>
-      <p>Biokovska 4, Split</p>
-      <p>OIB:36125969444</p>
       <div className={classes.inner}>
+        <div className={classes.flexCol}>
+          <div className={classes.company}>
+            <h3>OBRAZ d.o.o.</h3>
+            <p>Biokovska 4, Split</p>
+            <p>OIB:36125969444</p>
+          </div>
+          <div className={classes.fantaz}>
+            <p>
+              designed &
+              <br />
+              developed
+              <br />
+              <a href='mailto:fantaz@outlook.com'>by fantaz</a>
+            </p>
+          </div>
+        </div>
+
         <div className={classes.copyright}>
           <p>Copyright © 2025 Felix boats - All Rights Reserved</p>
-        </div>
-        <div className={classes.fantaz}>
-          <p>designed and developed</p>
-          <a href='mailto:fantaz@outlook.com'>
-            <p>
-              fantaz<span>.</span>
-            </p>
-          </a>
         </div>
       </div>
     </div>
